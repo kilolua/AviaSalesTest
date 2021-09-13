00a0bddd274c0e00b469039f1ef2de6a9b65e25e
ngladkov <ngladkov@tm-serv.ru>
2021-09-13 23:57:56 +0300

    add Header

---
+++ avia-sales-test/src/componets/Header.js
@@ -0,0 +1,15 @@
+
+function Header() {
+  const styles = {
+    width:'100%',
+    background:'red',
+    height:'3em',
+  }
+  return (
+    <div style={styles}>
+      HEADER
+    </div>
+  );
+}
+
+export default Header;
